78e74a7f6eb29f1602fac0a66efe75b624110103
LAPTOP-B29GO558\adamf <adamfinkler6@gmail.com>
2025-04-01 13:26:42 +0300

    improved string cleaning utils to some more edge cases and updated custom hook accordingly

--- next-insurance-frontend/src/utils/utils.ts
+++ next-insurance-frontend/src/utils/utils.ts
@@ -1,28 +1,25 @@
-import DOMPurify from 'dompurify';
-
-export const cleanString = (string: string) => {
-    return DOMPurify.sanitize(string, { ALLOWED_TAGS: [] }); 
-  };
+import DOMPurify from "dompurify";
 
 
-  export const cleanSynopsis = (string: string): string => {
-
-    const sanitized = DOMPurify.sanitize(string);
-    
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(sanitized, 'text/html');
-  
-    let result = '';
-    doc.body.childNodes.forEach((node) => {
-      if (node.nodeType === Node.TEXT_NODE) {
-        result += node.textContent;
-      }
-    });
-    
-    return cleanString(result).trim();
-  };
+export const decodeHtmlEntities = (input: string): string => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(input, "text/html");
+  return doc.documentElement.textContent || "";
+};
 
 
- export const hasRating = (string: string):boolean  =>  {
-    return string.trim().length > 0
-  }
+export const cleanString = (input: string): string => {
+  const sanitized = DOMPurify.sanitize(input, { ALLOWED_TAGS: [] });
+  return decodeHtmlEntities(sanitized).trim();
+};
+
+
+export const cleanSynopsis = (input: string): string => {
+  const stringAfterRegex = input.replace(/<br>\s*<b>New on[^<]*<\/b>/i, "");
+  const sanitized = DOMPurify.sanitize(stringAfterRegex);
+  return decodeHtmlEntities(sanitized).trim();
+};
+
+export const hasRating = (string: string): boolean => {
+  return string.trim().length > 0;
+};
