38607c9c030f9c7620d173a0829918e14bc0f26c
LAPTOP-B29GO558\adamf <adamfinkler6@gmail.com>
2025-04-06 16:36:21 +0300

    added request, a custome hook, data to the store and a change to the card component in order to implement the recommendation feature

--- next-insurance-frontend/src/common-components/card/Card.tsx
+++ next-insurance-frontend/src/common-components/card/Card.tsx
@@ -5,8 +5,10 @@ import starIcon from "../../assets/star.svg";
 import arrowIcon from "../../assets/arrow.png";
 import CardButton from "../card-button/CardButton";
 import Modal from "../modal/Modal";
+import { useMovieStore } from "../../store/movieStore";
 
 const Card = ({
+  id,
   imageUrl,
   title,
   released,
@@ -15,7 +17,8 @@ const Card = ({
   duration,
 }: ICard) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const updateCash = useMovieStore((state) => state.updateCashedMovieIds);
+  const cash = useMovieStore((state) => state.cashedMovieIds);
   const setOpenModal = () => {
     setIsModalOpen(true);
   };
@@ -40,7 +43,11 @@ const Card = ({
       <CardButton
         text="Read more"
         icon={arrowIcon}
-        onClickHandler={setOpenModal}
+        onClickHandler={() => {
+          setOpenModal();
+          updateCash(id);
+          console.log("cashed items are: ", cash);
+        }}
         direction="right"
       />
 
--- next-insurance-frontend/src/store/movieStore.ts
+++ next-insurance-frontend/src/store/movieStore.ts
@@ -1,30 +1,64 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { IMovie } from "./types";
 
 interface MoviesStore {
   movies: IMovie[];
   searchTerm: string;
   pageIndex: number;
-  sortedByRating: boolean;
+  isSortedByRating: boolean;
+  isShowingRecommended: boolean;
   filteredMovies: IMovie[];
+  recommendedMovies: IMovie[];
+  cashedMovieIds: string[];
+  updateCashedMovieIds: (newId: string) => void;
+  setCashedMovieIds: (movieIds: string[]) => void;
+  setRecommendedMovies: (movies: IMovie[]) => void;
   setPageIndex: (pageIndex: number) => void;
   setMovies: (movies: IMovie[]) => void;
   setFilteredMovies: (movies: IMovie[]) => void;
   setSearchTerm: (term: string) => void;
-  toggleSortByRating: () => void;
+  toggleIsSortByRating: () => void;
+  toggleIsShowingRecommended: () => void;
 }
 
-export const useMovieStore = create<MoviesStore>()((set) => ({
-  movies: [],
-  searchTerm: "",
-  pageIndex: 0,
-  sortedByRating: false,
-  filteredMovies: [],
-  setPageIndex: (pageIndex: number) => set({ pageIndex }),
-  setMovies: (newMovies: IMovie[]) => set({ movies: newMovies }),
-  setFilteredMovies: (newMovies: IMovie[]) =>
-    set({ filteredMovies: newMovies }),
-  setSearchTerm: (term: string) => set({ searchTerm: term }),
-  toggleSortByRating: () =>
-    set((state) => ({ sortedByRating: !state.sortedByRating })),
-}));
+export const useMovieStore = create<MoviesStore>()(
+  persist(
+    (set, get) => ({
+      movies: [],
+      searchTerm: "",
+      pageIndex: 0,
+      isSortedByRating: false,
+      isShowingRecommended: false,
+      filteredMovies: [],
+      recommendedMovies: [],
+      cashedMovieIds: [],
+      updateCashedMovieIds: (newId: string) => {
+        const currentMovieIds = get().cashedMovieIds;
+        const filteredIds = currentMovieIds.filter((id) => id !== newId);
+        filteredIds.push(newId);
+        if (filteredIds.length > 5) {
+          filteredIds.shift();
+        }
+        set({ cashedMovieIds: filteredIds });
+      },
+      setCashedMovieIds: (newMovieIds: string[]) =>
+        set({ cashedMovieIds: newMovieIds }),
+      setRecommendedMovies: (newMovies: IMovie[]) =>
+        set({ recommendedMovies: newMovies }),
+      setPageIndex: (pageIndex: number) => set({ pageIndex }),
+      setMovies: (newMovies: IMovie[]) => set({ movies: newMovies }),
+      setFilteredMovies: (newMovies: IMovie[]) =>
+        set({ filteredMovies: newMovies }),
+      setSearchTerm: (term: string) => set({ searchTerm: term }),
+      toggleIsSortByRating: () =>
+        set((state) => ({ isSortedByRating: !state.isSortedByRating })),
+      toggleIsShowingRecommended: () =>
+        set((state) => ({ isShowingRecommended: !state.isShowingRecommended }))
+    }),
+    {
+      name: "cashedMovieIdsStore", 
+      partialize: (state) => ({ cashedMovieIds: state.cashedMovieIds })
+    }
+  )
+);
